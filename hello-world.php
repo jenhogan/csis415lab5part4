56a5e30ca7167d8a10c7a0886f000394e845c05a
Meg Fryling <mfryling@siena.edu>
2017-02-23 15:55:59 -0500

    Add files via upload

---
+++ hello-world.php
@@ -0,0 +1,11 @@
+<html><body>
+<h1>Lab 5</h1>
+<!-- Display your form's new input. -->
+<?php
+    $first = $_GET["first"];
+    $last = $_GET["last"];
+
+    echo "<p>Hello $first $last!</p>"; 
+?>
+</body>
+</html>
